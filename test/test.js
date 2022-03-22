676282918d49ab55aeb6e603c157148745b2b882
MJ LEE <mj1201276@yahoo.com.au>
2022-03-23 00:10:10 +1000

    Fixed test file

--- test/test.js
+++ test/test.js
@@ -4,8 +4,8 @@ const { ethers } = require("hardhat");
 describe("NFtminter", function () {
   it("Nftminter deployed", async function () {
     const NftMinter = await ethers.getContractFactory("NFTminter");
-    const nftminter = await Greeter.deploy();
+    const nftminter = await NftMinter.deploy();
     await nftminter.deployed();
-
+    console.log("Contract Deployed")
   });
 });
